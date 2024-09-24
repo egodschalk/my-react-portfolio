f43e5031dc0857f2c0a0066a3642e31ac9091779
egodschalk <egodschalk@gmail.com>
2024-09-23 23:29:02 -0400

    navbar size fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
             <div className='about-content'>
                 <img className="headshot" src={headshot} alt="" />
                 <p className='about-text'>
-                    Hi, I’m Elizabeth Godschalk, a freshly minted Web Developer, thanks to UC Berkeley's Full Stack Bootcamp! Before diving into the world of coding, I spent 15 years mastering Product Management, Industrial Engineering, and Project Management. When I’m not geeking out over code, I’m twirling around the dance floor. I have a huge passion for Latin dance and love traveling the globe to learn from pros, dance with amazing people from every corner, and perform whenever I can.
+                    Hey there! I’m Elizabeth Godschalk, a freshly minted Web Developer, thanks to UC Berkeley's Full Stack Bootcamp. Before diving into the world of coding, I spent 15 years mastering Product Management, Industrial Engineering, and Project Management. When I’m not geeking out over code, I’m twirling around the dance floor. I have a huge passion for Latin dance and love traveling the globe to learn from pros, dance with amazing people from every corner, and perform whenever I can.
                 </p>
             </div>
         </div>
