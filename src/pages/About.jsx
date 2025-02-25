6a15aa2b17799495615eb8f275e11bd0049f9d34
egodschalk <egodschalk@gmail.com>
2025-02-25 16:50:46 -0500

    fixed menu issue on about screen 500-1000px width

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,6 @@ import headshot from '../assets/headshot2.jpg'
 export default function About() {
     return (
         <div className="about-page">
-            {/* <h2 className="about-header">About Me</h2> */}
             <div className='about-content'>
                 <img className="headshot" src={headshot} alt="" />
                 <p className='about-text'>
