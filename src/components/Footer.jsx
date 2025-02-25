0acc1e4488238fd58663b84645bddb370b236bad
egodschalk <egodschalk@gmail.com>
2025-02-25 16:18:05 -0500

    added contact page and new github icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import { Link, useLocation } from 'react-router-dom';
 import './Footer.css';
-import gitIcon from '../assets/github-logo.png'
+import gitIcon from '../assets/github-icon.png'
+import emailIcon from '../assets/mail-icon.png'
 import liIcon from '../assets/linkedin-icon.png'
-import igIcon from '../assets/instagram-icon.png'
-
+// import igIcon from '../assets/instagram-icon.png'
 
 function Footer() {
     return (
@@ -11,12 +11,18 @@ function Footer() {
             <a href="https://github.com/egodschalk">
                 <img className='git-icon-footer' src={gitIcon} />
             </a>
+            <a>
+                <Link to="/Contact" >
+                    <img src={emailIcon} alt="" className='contact-footer'
+                    />
+                </Link>
+            </a>
             <a href="https://www.linkedin.com/in/elizabeth-godschalk-6932202/">
                 <img className='li-icon-footer' src={liIcon} />
             </a>
-            <a href="https://www.instagram.com/elizabeth.rose.00/">
+            {/* <a href="https://www.instagram.com/elizabeth.rose.00/">
             <img className='ig-icon-footer' src={igIcon} />
-            </a>
+            </a> */}
         </div>
     );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,29 +1,58 @@
 import './Contact.css';
 import { useState, useEffect } from 'react';
+import Swal from 'sweetalert2';
 
 export default function Contact() {
+    
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "6f3b29ad-ad8f-4442-ade9-2511bbdf0354");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+       
+        if (res.success) {
+            Swal.fire({
+                title: "Thank you!",
+                text: "Your message has been sent",
+                icon: "success"
+              });
+            // console.log("Success", res);
+          }
+        };
+    
     return (
         <div className="contact-page">
-            <h2 className="contact-header">Contact</h2>
+            <h2 className="contact-header">Contact Me</h2>
             <div className='contact-content'>
-                <form className='form'>
+                <form onSubmit={onSubmit} className='form'>
                     <div className='field'>
-                        <label className='label'>Name:
-                            <input type="text" className="name" name="name" placeholder="what's your name?" />
+                        <label className='label'>
+                            <input type="text" className="name" name="name" placeholder="name" required/>
                         </label>
                     </div>
                     <div className='field'>
-                        <label className='label'>Email:
-                            <input type="text" className="email" name="email" placeholder="what's your email address?" />
+                        <label className='label'>
+                            <input type="text" className="email" name="email" placeholder="email" required/>
                         </label>
                     </div>
                     <div className='field'>
-                        <label className='label-message'>Message:
-                            <textarea className="message" rows="8" cols="50" placeholder="type your message here"></textarea>
-
+                        <label className='label-message'>
+                            <textarea className="message" name='message' placeholder="message" required></textarea>
                         </label>
                     </div>
-                    <button>Contact Me</button>
+                    <button type='submit'>Contact Me</button>
                 </form>
             </div>
         </div>
