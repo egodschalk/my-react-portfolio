f36e7582672ad529e94fefc3b0207a2c4bdd23ba
egodschalk <egodschalk@gmail.com>
2025-03-14 23:51:45 -0400

    fixed portfolio page styling

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,9 +9,9 @@ export default function Project({ data }) {
                 <img className='project-image' src={data.imgUrl} />
                 </a>
                 <div className='project-info'>
-                    {/* <a href={data.projectUrl}> */}
+                    <a href={data.projectUrl}>
                         <h3 className='project-title'>{data.title}</h3>
-                    {/* </a> */}
+                    </a>
                     <a href={data.gitUrl}>
                     <img className='git-logo' src={data.icon} />
                     </a>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,8 @@
 import './About.css'
 import headshot from '../assets/headshot2.jpg'
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Resume from './Resume';
 
 export default function About() {
     return (
@@ -10,6 +13,8 @@ export default function About() {
                     Hey there! I’m Elizabeth Godschalk, a freshly minted Web Developer, thanks to UC Berkeley's Full Stack Bootcamp. Before diving into the world of coding, I spent 15 years mastering Product Management, Industrial Engineering, and Project Management. When I’m not geeking out over code, I’m twirling around the dance floor. I have a huge passion for Latin dance and love traveling the globe to learn from pros, dance with amazing people from every corner, and perform whenever I can.
                 </p>
             </div>
+            {/* <Portfolio /> */}
+            {/* <Resume /> */}
         </div>
     );
 }
